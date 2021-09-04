0c876b7c82108616cace50f84fe72ea9b237b610
ImGabreuw <imgabreuw.dev@gmail.com>
2021-09-04 14:54:16 -0300

    Configuração do Apache Avro

---
+++ checkout/src/test/java/me/gabreuw/checkout/CheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabreuw.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
